c9a07baf54ce1d7cd782708eb318411fe057c8dd
ZOU\start <atit@qq.com>
2026-03-29 21:29:39 +0800

    修复空白页bug和页面不校验的bug

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="not-found-container">
+    <div class="not-found-content">
+      <h1 class="error-code">404</h1>
+      <p class="error-message">抱歉，您访问的页面不存在</p>
+      <el-button type="primary" @click="goHome">返回首页</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goHome = () => {
+  router.push('/')
+}
+</script>
+
+<style scoped>
+.not-found-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - 60px);
+  background-color: #f5f7fa;
+}
+
+.not-found-content {
+  text-align: center;
+  padding: 40px;
+}
+
+.error-code {
+  font-size: 120px;
+  font-weight: bold;
+  color: #409eff;
+  margin: 0 0 20px 0;
+  line-height: 1;
+}
+
+.error-message {
+  font-size: 18px;
+  color: #606266;
+  margin-bottom: 30px;
+}
+</style>
